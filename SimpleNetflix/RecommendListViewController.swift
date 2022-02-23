817d1f275473160e276418eafe49f134cbaf5e40
minjiLee68 <dlalswl2598@gmail.com>
2022-02-24 00:52:40 +0900

    .

--- SimpleNetflix/RecommendListViewController.swift
+++ SimpleNetflix/RecommendListViewController.swift
@@ -8,22 +8,44 @@
 import UIKit
 
 class RecommendListViewController: UIViewController {
-
+    @IBOutlet weak var sectionTitle: UILabel!
+    var viewModel = RecommentListViwModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
     }
-    */
+    
+    func updateUI() {
+        
+    }
+}
 
+class RecommentListViwModel {
+    enum RecommendingType{
+        case award
+        case hot
+        case my
+        
+        var title: String {
+            switch self {
+            case .award:
+                return "아카데미 호평 영화"
+            case .hot:
+                return "취향저격 HOT 콘텐츠"
+            case .my:
+                return "내가 찜한 콘텐츠"
+            }
+        }
+    }
+    
+    private (set) var type: RecommendingType = .my
+    private var items: [DummItem] = []
+}
+
+struct DummItem {
+    let thumbNail: UIImage
 }
--- SimpleNetflix/SearchViewController.swift
+++ SimpleNetflix/SearchViewController.swift
@@ -8,22 +8,19 @@
 import UIKit
 
 class SearchViewController: UIViewController {
-
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var resultCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        // 검색 시작
+        
+        print("--> 검색어: \(searchBar.text)")
     }
-    */
-
 }
