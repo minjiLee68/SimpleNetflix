d9d018177835d4e37b1e12ce56636361914a2e69
minjiLee68 <dlalswl2598@gmail.com>
2022-03-02 15:58:05 +0900

    collectionView , thumbnailImagePath

--- SimpleNetflix/SearchViewController.swift
+++ SimpleNetflix/SearchViewController.swift
@@ -6,14 +6,54 @@
 //
 
 import UIKit
+import Kingfisher
 
 class SearchViewController: UIViewController {
     @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var resultCollectionView: UICollectionView!
+    
+    var movies: [Movie] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
+}
+
+extension SearchViewController: UICollectionViewDataSource {
+    // 몇개 넘어오나
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movies.count
+    }
+    // 어떻게 표현하나
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ResultCell", for: indexPath) as? ResultCell else { return UICollectionViewCell() }
+        
+        let movie = movies[indexPath.item]
+        let url = URL(string: movie.thumbnailPath)!
+        cell.movieThumnail.kf.setImage(with: url)
+        
+        return cell
+    }
+}
+
+extension SearchViewController: UICollectionViewDelegate {
+    
+}
+
+extension SearchViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let margin: CGFloat = 8
+        let itemSpacing: CGFloat = 10
+        let width = (collectionView.bounds.width - margin * 2 - itemSpacing * 2) / 3
+        let height = width * 10/7
+        return CGSize(width: width, height: height)
+    }
+}
+
+class ResultCell: UICollectionViewCell {
+    @IBOutlet weak var movieThumnail: UIImageView!
 }
 
 extension SearchViewController: UISearchBarDelegate {
@@ -31,13 +71,17 @@ extension SearchViewController: UISearchBarDelegate {
         
         // 네트워킹을 통한 검색
         // - 목표: 서치텀을 가지고 네트워킹을 통해서 영화 검색
-        // - 검색 API 가 필요
-        // - 결과를 받아올 모델 Movie, Response
-        // - 결과를 받아와서, CollectionView로 표현해주자
+        // - [x] 검색 API 가 필요
+        // - [x] 결과를 받아올 모델 Movie, Response
+        // - [x] 결과를 받아와서, CollectionView로 표현해주자
         
         SearchAPI.search(searcgTerm) { movies in
             // collectionView로 표현하기
-            print("--> 몇개 넘어왔어? \(movies.count), 첫번째꺼 제목: \(movies.first?.title)")
+            DispatchQueue.main.async {
+                self.movies = movies
+                self.resultCollectionView.reloadData()
+            }
+            
         }
     }
 }
